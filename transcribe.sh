60790762c36db9450ac7b809fa43ac5c47436b90
Owen Raymond <owenbraymond@gmail.com>
2018-05-16 19:03:44 -0700

    Update transcribe.sh

--- transcribe.sh
+++ transcribe.sh
@@ -4,6 +4,7 @@ echo 'Running transcribe'
 
 : "${API_KEY?You need to set the API Key}"
 
-curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json \
-"https://speech.googleapis.com/v1beta1/speech:longrunningrecognize?key=${API_KEY}"
+NAME = $(curl -s -X POST -H "Content-Type: application/json" --data-binary @request.json \
+"https://speech.googleapis.com/v1beta1/speech:longrunningrecognize?key=${API_KEY}")
 
+curl "https://speech.googleapis.com/v1/operations/${NAME}?key=${API_KEY}"
